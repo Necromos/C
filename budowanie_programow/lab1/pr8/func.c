9c1d573fd6cac5252632b925084e4b0e75a5f795
Necromos <masternecromos@gmail.com>
2011-03-15 14:03:34 +0100

    niedokonczony prog

---
+++ budowanie_programow/lab1/pr8/func.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int wczytaj (void) {
+int a;
+scanf("%i", &a);
+return a;
+}
+
+int suma (int a, int b, int c) {
+int s;
+return s=a+b+c;
+}
+
+int iloczyn (int a, int b, int c) {
+int i;
+return i=a*b*c;
+}
+
+int naj(int a, int b, int c) {
+int najm;
+if (a<b)
+	if (b<c)
+		najm=a;
+if (b<a)
+	if (a<c)
+		najm=b;
+if (c<a)
+
+
+}
