35d14997f7231f3e20bc687c748a50668e6fb6e9
Necromos <masternecromos@gmail.com>
2011-05-10 16:27:36 +0200

    Update podczas cwiczen

---
+++ wskazniki/wsk.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* oblicz kwadrat liczby n */
+void kwadrat(int *n){
+}
+void wczytaj_liczbe(int *n){
+    printf("Wpisz liczbę naturalną: ");
+}
+
+int main()
+{
+    int liczba=2, wynik=4;
+    wczytaj_liczbe(&liczba);
+    kwadrat(&wynik);
+    printf("\nPodano liczbe: %d, jej kwadrat: %d\n", liczba, wynik);
+    return 0;
+}
