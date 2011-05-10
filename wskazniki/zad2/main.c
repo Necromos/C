823d35a6442a8623c907803674646ed0e98cc352
Necromos <masternecromos@gmail.com>
2011-05-10 16:50:49 +0200

    Update podczas laboratoriow

--- wskazniki/zad2/main.c
+++ wskazniki/zad2/main.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 
 
-int main() {
-	int t[4] = {2, 8, 1, 3};
-	int *a[4];
-	int i;	
-	for (i = 0; i < 4; i++){
-	printf("%d", t[i]);
-	}
-	printf("\n");
-	// wyniki
-	for (i=0; i< 4; i++) {
-		printf("%d ", *a[i]);
-	}
-	printf("\n");
+int main()
+{
+    int t[4] = { 2, 8, 1, 3 };
+    int *a[4];
+    int i;
 
-	return 0;
+    for (i = 0; i < 4; i++) {
+	printf("%d", t[i]);
+    }
+    printf("\n");
+
+
+    for (i = 0; i < 4; i++) {
+	a[i] = t[i];
+    }
+    printf("\n");
+
+    for (i = 0; i < 4; i++) {
+	printf("%d ", *a[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
