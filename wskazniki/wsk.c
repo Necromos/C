48174545dc89caf3add64d88e479e2d25865c52a
Necromos <masternecromos@gmail.com>
2011-05-10 16:30:12 +0200

    Update podczas laboratoriow

--- wskazniki/wsk.c
+++ wskazniki/wsk.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 /* oblicz kwadrat liczby n */
-void kwadrat(int *n){
+void kwadrat(int m, int *n){
+
 }
+
 void wczytaj_liczbe(int *n){
     printf("Wpisz liczbę naturalną: ");
 }
@@ -11,7 +13,7 @@ int main()
 {
     int liczba=2, wynik=4;
     wczytaj_liczbe(&liczba);
-    kwadrat(&wynik);
+    kwadrat(liczba, &wynik);
     printf("\nPodano liczbe: %d, jej kwadrat: %d\n", liczba, wynik);
     return 0;
 }
