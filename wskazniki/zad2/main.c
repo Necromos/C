e1296d7843c2812e5c3e3edb8bcddf747ecff0d1
Necromos <masternecromos@gmail.com>
2011-05-10 16:47:01 +0200

    Update podczas laboratoriow

--- wskazniki/zad2/main.c
+++ wskazniki/zad2/main.c
@@ -2,12 +2,18 @@
 
 
 int main() {
-	int t[] = {2, 8, 1, 3};
+	int t[4] = {2, 8, 1, 3};
 	int *a[4];
 	int i;	
 	for (i = 0; i < 4; i++){
 	printf("%d", t[i]);
 	}
 	printf("\n");
+	// wyniki
+	for (i=0; i< 4; i++) {
+		printf("%d ", *a[i]);
+	}
+	printf("\n");
+
 	return 0;
 }
